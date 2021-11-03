4f04bde0500c427d1de3f885098e5477dbb86b6d
Karthik143073 <89535557+Karthik143073@users.noreply.github.com>
2021-11-03 17:02:02 -0500

    g

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/inhaler', inhalerRouter);
 app.use('/addmods', addmodsRouter);
+app.use('/selector', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
