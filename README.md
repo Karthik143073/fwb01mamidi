5cdbb4cd32e0db40b05c0287ceb50a010d9bd11b
Karthik143073 <89535557+Karthik143073@users.noreply.github.com>
2021-11-03 16:23:52 -0500

    files

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# fwb01mamidi
-hello world
+deploying server into heroku
+
+hosted at
+
+Alphabets - I, J, K, L
+
+Class Selected:
+
+1. Inhaler manufacturedYear(number) , brandName(string), price(number)
+
+2. Jockey  material(string), color(string), validity(number)
+
+3. Kite material(string), color(string), price(number)
+
+4. Language  found(number), place(string), population(number)
--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var inhalerRouter = require('./routes/inhaler');
+var addmodsRouter = require('./routes/addmods');
 var app = express();
 
 // view engine setup
@@ -21,6 +22,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/inhaler', inhalerRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
